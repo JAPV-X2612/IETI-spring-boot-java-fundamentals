03b0c2dd7a935e350357626da0ed4ad9a53efe20
JAPV-X2612 <jesus.pinzon-v@mail.escuelaing.edu.co>
2026-03-12 01:50:14 -0500

    feat: Add new checkAPI endpoint

---
+++ src/main/java/edu/eci/ieti/controller/health/HealthController.java
@@ -0,0 +1,22 @@
+package edu.eci.ieti.controller.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * REST controller for health check endpoints
+ *
+ * @author Jesús Pinzón
+ * @version 1.0
+ * @since 2026-03-12
+ */
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping
+    public String checkAPI() {
+        return "<h1>The API is working ok!</h1>";
+    }
+}
