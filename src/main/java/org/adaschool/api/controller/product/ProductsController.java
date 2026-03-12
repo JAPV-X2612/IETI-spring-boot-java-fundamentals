3608a445a7414b91d6f77142bd2ba7ae9d7ce084
JAPV-X2612 <jesus.pinzon-v@mail.escuelaing.edu.co>
2026-03-12 01:59:22 -0500

    refactor: Implement endpoints logic

--- src/main/java/org/adaschool/api/controller/product/ProductsController.java
+++ src/main/java/org/adaschool/api/controller/product/ProductsController.java
@@ -1,6 +1,8 @@
 package org.adaschool.api.controller.product;
 
+import org.adaschool.api.exception.ProductNotFoundException;
 import org.adaschool.api.repository.product.Product;
+import org.adaschool.api.repository.product.ProductDto;
 import org.adaschool.api.service.product.ProductsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,7 +10,15 @@ import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
+/**
+ * REST controller for products endpoints
+ *
+ * @author Jesús Pinzón
+ * @version 1.0
+ * @since 2026-03-12
+ */
 @RestController
 @RequestMapping("/v1/products/")
 public class ProductsController {
@@ -20,34 +30,47 @@ public class ProductsController {
     }
 
     @PostMapping
-    public ResponseEntity<Product> createProduct() {
-        //TODO implement this method
-        URI createdProductUri = URI.create("");
-        return ResponseEntity.created(createdProductUri).body(null);
+    public ResponseEntity<Product> createProduct(@RequestBody ProductDto productDto) {
+        Product product = new Product(productDto);
+        Product savedProduct = productsService.save(product);
+        URI createdProductUri = URI.create("/v1/products/" + savedProduct.getId());
+        return ResponseEntity.created(createdProductUri).body(savedProduct);
     }
 
     @GetMapping
     public ResponseEntity<List<Product>> getAllProducts() {
-        //TODO implement this method
-        return ResponseEntity.ok(null);
+        List<Product> products = productsService.all();
+        return ResponseEntity.ok(products);
     }
 
     @GetMapping("{id}")
     public ResponseEntity<Product> findById(@PathVariable("id") String id) {
-        //TODO implement this method
-        return ResponseEntity.ok(null);
+        Optional<Product> product = productsService.findById(id);
+        if (!product.isPresent()) {
+            throw new ProductNotFoundException(id);
+        }
+        return ResponseEntity.ok(product.get());
     }
 
-    @PutMapping
-    public ResponseEntity<Product> updateProduct() {
-        //TODO implement this method
-        return ResponseEntity.ok(null);
+    @PutMapping("{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable("id") String id, @RequestBody ProductDto productDto) {
+        Optional<Product> existingProduct = productsService.findById(id);
+        if (!existingProduct.isPresent()) {
+            throw new ProductNotFoundException(id);
+        }
+        Product product = existingProduct.get();
+        product.update(productDto);
+        Product updatedProduct = productsService.save(product);
+        return ResponseEntity.ok(updatedProduct);
     }
 
-    @DeleteMapping
-    public ResponseEntity<Void> deleteProduct() {
-        //TODO implement this method
-
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable("id") String id) {
+        Optional<Product> product = productsService.findById(id);
+        if (!product.isPresent()) {
+            throw new ProductNotFoundException(id);
+        }
+        productsService.deleteById(id);
         return ResponseEntity.ok().build();
     }
 }
