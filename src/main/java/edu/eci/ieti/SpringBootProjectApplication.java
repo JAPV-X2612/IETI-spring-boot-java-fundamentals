da303197ab28492f0fd2e20d9df1672b7c770ac8
JAPV-X2612 <jesus.pinzon-v@mail.escuelaing.edu.co>
2026-03-12 01:50:25 -0500

    feat: Add new SpringBootProjectApplication class

---
+++ src/main/java/edu/eci/ieti/SpringBootProjectApplication.java
@@ -0,0 +1,19 @@
+package edu.eci.ieti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main application class for Spring Boot Project
+ *
+ * @author Jesús Pinzón
+ * @version 1.0
+ * @since 2026-03-12
+ */
+@SpringBootApplication
+public class SpringBootProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootProjectApplication.class, args);
+    }
+}
